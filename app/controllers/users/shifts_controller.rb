1e49ff4ddf752bedee70524c9a3bd3c33bc15a1d
massive2351 <massive2351@gmail.com>
2021-05-05 00:42:26 +0000

    [add]ユーザー：シフﾄ一覧、トップ

--- app/controllers/users/shifts_controller.rb
+++ app/controllers/users/shifts_controller.rb
@@ -1,2 +1,22 @@
 class Users::ShiftsController < ApplicationController
+  
+  def top
+    @shifts = Shift.all
+    @shift_today = Shift.where("DATE(start_time) = '#{Date.today}'")
+  end
+  
+  def index
+    @shifts = Shift.includes(:user)
+  end
+  
+  def show
+    @shift = Shift.find(params[:id])
+    @shifts = Shift.all
+  end
+  
+  private
+  def shift_params
+    params.require(:shift).permit(:start_time, :end_time, :type, :work, :staff, :user_id, :customer_id, :customer_na)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,9 @@ Rails.application.routes.draw do
 
   scope module: :users do
     root :to => 'homes#top'
+    get '/top' => 'shifts#top'
+    resources :customers
+    resources :shifts
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
