91f76b7f73e763ea9ceb6dbb850948339d69b8f8
massive2351 <massive2351@gmail.com>
2021-05-09 08:38:48 +0000

    草生やすPart2

--- config/routes.rb
+++ config/routes.rb
@@ -48,5 +48,8 @@ Rails.application.routes.draw do
     resources :records
     resources :informations
   end
+  
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
