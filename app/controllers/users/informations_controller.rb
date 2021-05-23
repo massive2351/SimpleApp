074addae444c4c58b3d37c71f488db8f468548cf
massive2351 <massive2351@gmail.com>
2021-05-23 05:28:53 +0000

    [edit]informationの並び方を最新順

--- app/controllers/users/informations_controller.rb
+++ app/controllers/users/informations_controller.rb
@@ -1,8 +1,10 @@
 class Users::InformationsController < ApplicationController
   before_action :authenticate_user!
   layout 'users'
+  
   def index
-    @informations = Information.all
+    #最新の順番
+    @informations = Information.all.order("created_at DESC")
   end
   
 end
